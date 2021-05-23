76935cddcd3caaf087d70af7a0c0b35a5095f917
cparros <celparros@gmail.com>
2021-05-23 17:38:57 -0400

    added product component with info aprice stars and image and rendered on page. Stying next

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style/Header.css";
+import "./styles/Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
