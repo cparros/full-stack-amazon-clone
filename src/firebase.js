08232de43ec9fc7c9b289c82a7f13f7592bc8553
cparros <celparros@gmail.com>
2021-05-23 14:37:06 -0400

    added firbase.js file with apikey authdomain etc

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// code snipet received from firebase project settings, general tab, under config sdk setup
+const firebaseConfig = {
+  apiKey: "AIzaSyALtnX83DkUsP7cBoqL8XWAZJD_WSwX5Ao",
+  authDomain: "clone-challenge-cd675.firebaseapp.com",
+  projectId: "clone-challenge-cd675",
+  storageBucket: "clone-challenge-cd675.appspot.com",
+  messagingSenderId: "826440329364",
+  appId: "1:826440329364:web:622306b65218ce19b7ac81"
+};
