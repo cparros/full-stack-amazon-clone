bd382ba31264220aef77440c63804af0c918b2d2
cparros <celparros@gmail.com>
2021-05-23 16:18:45 -0400

    added home component to app.js and ccreated home component and style files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
     //BEM name convention for styling
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <p>i am Home</p>
+    </div>
+  );
+}
+
+export default Home;
