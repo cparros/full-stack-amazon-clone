1d764a0cf0aae7169c82fa17f84a9e2979c60cbc
cparros <celparros@gmail.com>
2021-05-24 10:10:41 -0400

    added some functionality to capture email and password with use state and on change

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className="login">
       <Link to="/">
@@ -15,10 +18,10 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type='text' />
+          <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
 
           <h5>Password</h5>
-          <input type='password' />
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
 
           <button className='login__signInButton'>Sign In</button>
         </form>
