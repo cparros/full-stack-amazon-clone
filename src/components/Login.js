07b4dc39eafeeb64ed468aca85a7a1f37563e36f
cparros <celparros@gmail.com>
2021-05-24 11:14:46 -0400

    added login funcitonality with firebase on sign in and registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,44 @@
 import React, { useState } from "react";
 import "./styles/Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  //use history allows us to programatically change the url
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   //Register and sign in info from firebase site. Need config file info and Authentication setup. "get started/sign-in method. Enable email/password"
 
   //you will need to npm i firbase package OR sudo npm i -g firebase-tools for global install
   const signIn = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     //Firebase Login functionality
-  }
+    auth.signInWithEmailAndPassword(email, password)
+    .then(auth => {
+      history.push('/')
+    })
+    .catch(error => alert(error.message))
+  };
 
   const register = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     //Firebase Register functionality
-  }
- 
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //Successfully created user with email and password
+        console.log(auth);
+        //if user auth is true reroute to home page and push that into history
+        if (auth) {
+          history.push("/");
+        }
+      })
+      //Catch error
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -31,19 +51,37 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button onClick={signIn} type='submit' className='login__signInButton'>Sign In</button>
+          <button
+            onClick={signIn}
+            type="submit"
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
 
         <p>
-          By signing-in you agree to CPARROS AMAZON CLONE Condition of Use & Fun. Please see our nonexistent Provacy Notice, Cookie Notice and Interest-Based ads and remember to be KIND to everyone around you.
+          By signing-in you agree to CPARROS AMAZON CLONE Condition of Use &
+          Fun. Please see our nonexistent Provacy Notice, Cookie Notice and
+          Interest-Based ads and remember to be KIND to everyone around you.
         </p>
 
-        <button onClick={register} className='login__registerButton'>Create New Amazon Account</button>
+        <button onClick={register} className="login__registerButton">
+          Create New Amazon Account
+        </button>
       </div>
     </div>
   );
